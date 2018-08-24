7b8a66985a291fe5386fcefbef1a2a4160e44527
jsuyash1514 <jsuyash1514@gmail.com>
2018-08-25 04:32:53 +0530

    Adds Contact tab

--- app/src/main/java/ac/in/iitr/mdg/convocation/views/Contact.java
+++ app/src/main/java/ac/in/iitr/mdg/convocation/views/Contact.java
@@ -5,4 +5,32 @@ package ac.in.iitr.mdg.convocation.views;
  */
 
 public class Contact {
+    public String contact_category;
+    public ContactCard contactCard;
+    public static final int TYPE_CATEGORY = 0;
+    public static final int TYPE_CONTACT = 1;
+    private int type;
+
+    public Contact(int type, String contact_category) {
+        this.contact_category = contact_category;
+        this.type = type;
+    }
+
+    public Contact(int type, ContactCard contactCard) {
+        this.contactCard = contactCard;
+        this.type = type;
+    }
+
+    public String getContact_category() {
+        return contact_category;
+    }
+
+    public ContactCard getContactCard() {
+        return contactCard;
+    }
+
+    public int getType() {
+        return type;
+    }
+
 }
